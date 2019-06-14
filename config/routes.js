5382db160a88c291cdb1b62a3f4211630d5e57aa
Alyssa Jane <xcthaeh@gmail.com>
2019-06-14 14:13:04 -0400

    added first mvp route

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,40 @@
-const axios = require('axios');
-
-const { authenticate } = require('../auth/authenticate');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const db = require("../database/dbConfig");
+const jwtKey = require("../secret/secrets").jwtKey;
+const jwt = require("jsonwebtoken");
+const { authenticate } = require("../auth/authenticate");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
   // implement user registration
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 4);
+  creds.password = hash;
+
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => json(err));
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.usernam
+  };
+  const secret = jwtKey;
+  const options = {
+    expiresIn: "60m"
+  };
+  return jwt.sign(payload, secret, options);
 }
 
 function login(req, res) {
@@ -18,15 +43,15 @@ function login(req, res) {
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
