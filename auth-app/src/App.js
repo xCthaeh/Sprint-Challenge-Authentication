a81c607c71804a941688c1390f69a7d80eb431fd
Alyssa Jane <xcthaeh@gmail.com>
2019-06-14 14:46:24 -0400

    added routes in app.js

--- auth-app/src/App.js
+++ auth-app/src/App.js
@@ -1,22 +1,25 @@
 import React from "react";
+import { Route, NavLink } from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Jokes from "./components/Jokes";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <NavLink to="/">Sign Up</NavLink>
+        &nbsp; | &nbsp;
+        <NavLink to="/jokes">Jokes</NavLink>
+        &nbsp; | &nbsp;
+        <NavLink to="/login">Sign In</NavLink>
+      </nav>
+      <section>
+        <Route exact path="/" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/jokes" component={Jokes} />
+      </section>
     </div>
   );
 }
